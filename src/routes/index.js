53ff9518275951c3177a1d5834c8dccd668bd159
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-03-05 18:21:24 -0500

    base page, bootstrap update, +bootstrap icons

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 // import postRoutes from './posts.js';
+import * as path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const constructorMethod = (app) => {
   //   app.use('/posts', postRoutes);
-
-  app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not found" });
-  });
 };
 
 export default constructorMethod;
