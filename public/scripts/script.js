9a1aa343a3bc732b4f43fc5fe380aabfb2b2f024
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-03-25 18:59:47 -0400

    cleanup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -22,57 +22,6 @@ function load() {
     xhttp.open("GET", `/calendar/api/${nav}`, true);
     xhttp.send();
   }
-  // const day = dt.getDay();
-  // const month = dt.getMonth();
-  // const year = dt.getFullYear();
-  // const firstDayOfMonth = new Date(year, month, 1);
-  // const daysInMonth = new Date(year, month + 1, 0).getDate();
-  // const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
-  //   weekday: "long",
-  //   day: "numeric",
-  //   month: "long",
-  //   year: "numeric",
-  // });
-  // const monthDropwDown = document.getElementById("monthDropwDown");
-  // months.forEach((monthName) => {
-  //   let el = document.createElement("option");
-  //   el.textContent = monthName;
-  //   el.value = monthName;
-  //   monthDropwDown.appendChild(el);
-  // });
-  // monthDropwDown.selectedIndex = firstDayOfMonth.getMonth();
-  // const paddingDays = firstDayOfMonth.getDay();
-  // let weekrow = document.createElement("tr");
-  // weekrow.classList.add("table-row", "table-bordered");
-  // calendar.innerHTML = "";
-  // for (let i = 1; i <= daysInMonth + paddingDays; i++) {
-  //   const daySquare = document.createElement("td");
-  //   daySquare.classList.add("table-cell", "table-bordered");
-  //   if (i > paddingDays) {
-  //     daySquare.innerText = i - paddingDays;
-  //   } else {
-  //     daySquare.innerText =
-  //       new Date(year, month, 0).getDate() - paddingDays + i;
-  //     daySquare.classList.add("table-active");
-  //   }
-  //   weekrow.appendChild(daySquare);
-  //   if (i % 7 === 0 || i === daysInMonth + paddingDays) {
-  //     const weekrowlen = weekrow.cells.length;
-  //     if (i === daysInMonth + paddingDays && weekrowlen !== 7) {
-  //       for (let j = 0; j < 7 - weekrowlen; j++) {
-  //         const temp = document.createElement("td");
-  //         temp.classList.add("table-cell", "table-bordered", "table-active");
-  //         temp.innerText = new Date(year, month, 1 + j).getDate();
-  //         weekrow.appendChild(temp);
-  //       }
-  //     }
-  //     calendar.appendChild(weekrow);
-  //     if (i / 7 > 0) {
-  //       weekrow = document.createElement("tr");
-  //       weekrow.classList.add("table-row", "table-bordered");
-  //     }
-  //   }
-  // }
 }
 
 function navigationButtons() {
