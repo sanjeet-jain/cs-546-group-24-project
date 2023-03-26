165becb12e027fd6f219d702ee13c2c8433d33c9
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-03-26 04:26:10 -0400

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const constructorMethod = (app) => {
   app.use("/calendar", calendarRoutes);
-  app.use("/sample", sampleRoutes);
+  // app.use("/sample", sampleRoutes);
 
   app.use("*", (req, res) => {
     // we can set this to check for authorization and then send back to correct page !
