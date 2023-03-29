0f7c763ffecd3324b5aa1fdf657c0b8a5044d75e
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-03-29 19:02:48 -0400

    formatting fix

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,16 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const constructorMethod = (app) => {
+  app.use("/login", sampleRoutes);
   app.use("/calendar", calendarRoutes);
-  // app.use("/sample", sampleRoutes);
   app.use("/meeting", meetingRoutes);
+  app.get("/", (req, res) => {
+    res.render("aboutUs");
+  });
   app.use("*", (req, res) => {
     // we can set this to check for authorization and then send back to correct page !
-    res.redirect("/calendar");
+    const error = { error: "The Requested Page was not Found!" };
+    res.status(404).render("errors/error", { error: error });
   });
 };
 
