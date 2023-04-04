52253be61a45af4350551e23bc48b64bbfbffb80
sanjeet-jain <91623753+sanjeet-jain@users.noreply.github.com>
2023-04-03 23:25:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     name: "AuthCookie",
