23cdd1f4be725eac95e2a9270c749ecb6140f6ec
sanjeet-jain <91623753+sanjeet-jain@users.noreply.github.com>
2023-04-03 23:25:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const handlebarsInstance = exphbs.create({
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     name: "AuthCookie",
