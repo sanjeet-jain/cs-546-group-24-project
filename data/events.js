ea4ef06da995e0c79e0d97ebe90597affb71d80b
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-04-08 17:20:08 -0400

    Update events.js

--- data/events.js
+++ data/events.js
@@ -5,13 +5,13 @@ import notesDataFunctions from "./notes.js";
 
 const exportedMethods = {
   async getAllEvents(userId) {
-    const meeetings = await meetingsDataFunctions.getAll(userId);
+    const meetings = await meetingsDataFunctions.getAll(userId);
     const reminders = await reminderDataFunctions.getAllReminders(userId);
     const tasks = await tasksDataFunctions.getAllTasks(userId);
     const notes = await notesDataFunctions.getAll(userId);
     return {
       userId: userId,
-      meeetings: meeetings,
+      meeetings: meetings,
       reminders: reminders,
       tasks: tasks,
       notes: notes,
