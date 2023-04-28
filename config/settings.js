cd94808e542f281d169f6221c1d1f961ed736585
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-04-28 02:10:04 -0400

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,5 @@
 // YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
 export const mongoConfig = {
-  serverUrl:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://wolverine270023:rHPB5SfOqVNmZZj6@timewisecalendar.tahfkzg.mongodb.net/?retryWrites=true&w=majority",
+  serverUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/",
   database: "TimeWiseCalendar",
 };
