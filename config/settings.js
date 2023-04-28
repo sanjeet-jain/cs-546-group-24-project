22ca9b83642d0ed24e15a35551b3d1f7ee7b17c2
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-04-27 20:24:33 -0400

    updates for heroku

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,6 @@
 // YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
 export const mongoConfig = {
-  serverUrl: "mongodb://127.0.0.1:27017/",
+  serverUrl:
+    "mongodb+srv://wolverine270023:rHPB5SfOqVNmZZj6@timewisecalendar.tahfkzg.mongodb.net/?retryWrites=true&w=majority",
   database: "TimeWiseCalendar",
 };
