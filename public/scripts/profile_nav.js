475a476e1776c7e25d366fc934d004627319144b
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-05-01 14:00:52 -0400

    btn fix

--- public/scripts/profile_nav.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll(".changePass, .editProfile");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.preventDefault(); // prevent default link behavior
-    const href = button.getAttribute("href");
-    window.location.href = href;
-  });
-});
